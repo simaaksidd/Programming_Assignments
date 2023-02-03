a0bc477595a50a706dc9aea165ddbf738a0f5cf9
Julian Canales <juliancanalez@Julians-MBP.attlocal.net>
2023-02-03 17:49:17 -0600

    testing

--- A5/OfficeSpace.py
+++ A5/OfficeSpace.py
@@ -98,6 +98,8 @@ def uncontested_space (bldg, rect):
   return specialSpace
 uncontested_space(updateBuilding, (5,6,7,10))
 
+# testing to see if I can push
+
 '''
 # Input: office is a rectangle in the form of a tuple of 4 integers
 #        representing the whole office space
