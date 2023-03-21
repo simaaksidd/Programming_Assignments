7e2d40a86d173074e79793a64f00d34bbac3723b
ssidd02 <simaaksidd1@gmail.com>
2023-03-20 22:09:09 -0500

    update other assignments

--- A12/Boxes.py
+++ A12/Boxes.py
@@ -1,119 +1,117 @@
-
 #  File: Boxes.py
 
-#  Description:
+#  Description: Finds the largest number of boxes that can be nested 
+#  and the number of such sets of boxes that do fit
 
-#  Student Name: Simaak Siddiqi
+#  Student Name: Julian Canales
 
-#  Student UT EID: srs5826
+#  Student UT EID: jac22779 
 
-#  Partner Name:
+#  Partner Name: Simmak Siddiqi
 
-#  Partner UT EID:
+#  Partner UT EID: srs5826
 
 #  Course Name: CS 313E
 
-#  Unique Number: 
+#  Unique Number: 52038
 
-#  Date Created:
+#  Date Created: 3/4/2023
 
-#  Date Last Modified:
+#  Date Last Modified: 3/6/2023
 
 import sys
 
-# Input: nesting_list is a list of all nesting boxes. 
-#        Max_now is the max number of boxes that fit in eachother
-# Output: function returns the , the maximum number of boxes
-#         that fit inside each other and the number of such nesting
-#         sets of boxes
-def get_subsets(nesting_list, box_list, ni, largest):
-  if ni == 0:
-    return []
-  else:
-    for i in range(0, largest):
-      if nesting_list[i] == ni and does_fit(box_list[i], box_list[largest]):
-        return [box_list[i]] + get_subsets(nesting_list, box_list, ni-1, largest)
-
+# Initialize memo to store results of already computed subproblems
+memo = {}
 
 # Input: 2-D list of boxes. Each box of three dimensions is sorted
 #        box_list is sorted
 # Output: function returns two numbers, the maximum number of boxes
 #         that fit inside each other and the number of such nesting
 #         sets of boxes
-def nesting_boxes (box_list):
-  # The maximum number of boxes that fit inside each other
-  max_now = 0 
-  # a list of nesting boxes. Each index is the number of
-  #       boxes which nest in the respective box in box_list
-  memo = []
-  nesting_list = []
-  for i in range(len(box_list)):
-    # The first box always has the nesting value of one
-    if i == 0:
-      nesting_list.append(1)
-      continue
-    # Find all of the nesting values
-    for j in range(i-1, -1, -1):
-      if does_fit(box_list[j], box_list[i]):
-        nesting_list.append(nesting_list[j]+1)
-        break
-    # If the loop completes and no value is assigned,
-    #     then the nesting value is one
-    if len(nesting_list) != (i+1):
-      nesting_list.append(1)
+def nesting_boxes(box_list):
+    max_nesting = 0  # maximum number of boxes that fit inside each other
+    num_sets = 0  # number of sets of boxes that achieve the maximum nesting
+    n = len(box_list)
 
-  max_now = max(nesting_list)
-  hmm = get_subsets(nesting_list, box_list, max_now-1, nesting_list.index(max_now))
-  print(nesting_list)
-  print(hmm)
-  return max_now, 1
+    # Iterate through all boxes and compute the maximum nesting starting from each box
+    for i in range(n):
+        nesting, count = max_nesting_from_box(i, box_list)
+        if nesting > max_nesting:
+            max_nesting = nesting
+            num_sets = count
+        elif nesting == max_nesting:
+            num_sets += count
+
+    return max_nesting, num_sets
+
+# returns the maximum number of boxes that fit inside the box at index i
+# and the number of sets of such boxes
+def max_nesting_from_box(i, box_list):
+    # Check if we've already computed the result for this box
+    if i in memo:
+        return memo[i]
+
+    max_nesting = 1  # every box fits inside itself
+    count = 1  # at least one set of boxes fits inside each other
+    n = len(box_list)
+    for j in range(i+1, n):
+        if does_fit(box_list[i], box_list[j]):
+            nesting, set_count = max_nesting_from_box(j, box_list)
+            nesting += 1
+            if nesting > max_nesting:
+                max_nesting = nesting
+                count = set_count
+            elif nesting == max_nesting:
+                count += set_count
+
+    # Store the result in memo before returning
+    memo[i] = (max_nesting, count)
+    return memo[i]
 
 # returns True if box1 fits inside box2
-def does_fit (box1, box2):
-  return (box1[0] < box2[0] and box1[1] < box2[1] and box1[2] < box2[2])
+def does_fit(box1, box2):
+    return box1[0] < box2[0] and box1[1] < box2[1] and box1[2] < box2[2]
 
 def main():
-  # read the number of boxes 
-  line = sys.stdin.readline()
-  line = line.strip()
-  num_boxes = int (line)
-
-  # create an empty list for the boxes
-  box_list = []
-
-  # read the boxes from the file
-  for i in range (num_boxes):
+    # read the number of boxes
     line = sys.stdin.readline()
     line = line.strip()
-    box = line.split()
-    for j in range (len(box)):
-      box[j] = int (box[j])
-    box.sort()
-    box_list.append (box)
+    num_boxes = int(line)
 
-  # print to make sure that the input was read in correctly
-  print (box_list)
-  print()
+    # create an empty list for the boxes
+    box_list = []
 
-  # sort the box list
-  box_list.sort()
+    # read the boxes from the file
+    for i in range(num_boxes):
+        line = sys.stdin.readline()
+        line = line.strip()
+        box = line.split()
+        for j in range(len(box)):
+            box[j] = int(box[j])
+        box.sort()
+        box_list.append(box)
 
-  # print the box_list to see if it has been sorted.
-  print (box_list)
-  print()
+    # print to make sure that the input was read in correctly
+    #print(box_list)
+    #print()
 
-  # get the maximum number of nesting boxes and the
-  # number of sets that have that maximum number of boxes
-  #max_boxes, num_sets = nesting_boxes (box_list)
+    # sort the box list
+    box_list.sort()
 
-  fishy = nesting_boxes(box_list)
+    # print the box_list to see if it has been sorted.
+    #print(box_list)
+    #print()
 
-  # print the largest number of boxes that fit
-  #print (max_boxes)
+    # get the maximum number of nesting boxes and the
+    # number of sets that have that maximum number of boxes
+    max_boxes, num_sets = nesting_boxes(box_list)
 
-  # print the number of sets of such boxes
-  #print (num_sets)
+    # print the largest number of boxes that fit
+    print(max_boxes)
+
+    # print the number of sets of such boxes
+    print(num_sets)
 
 if __name__ == "__main__":
-  main()
-
+    main()
--- A13/Chess.py
+++ A13/Chess.py
@@ -1,15 +1,14 @@
-
 #  File: Chess.py
 
-#  Description:
+#  Description: This program will find all solutions to the n-Queens problem
 
-#  Student Name: Simaak Siddiqi
+#  Student Name: Julian Canales
 
-#  Student UT EID: srs5826
+#  Student UT EID: jac22779
 
-#  Partner Name: Julian Canales
+#  Partner Name: Simaak Siddiqi
 
-#  Partner UT EID: jac22779
+#  Partner UT EID: srs5826
 
 #  Course Name: CS 313E
 
@@ -30,6 +29,8 @@ class Queens (object):
       for j in range (self.n):
         row.append ('*')
       self.board.append (row)
+    # added an attribute that keeps track of the number of solutions
+    self.count = 0
 
   # print the board
   def print_board (self):
@@ -51,93 +52,27 @@ class Queens (object):
         if (row_diff == col_diff) and (self.board[i][j] == 'Q'):
           return False
     return True
-  
- # given a board is fully filled out check if a board is a valid solution 
-  def is_solution(self):
-    for i in range(self.n):
-      for j in range(self.n):
-        if self.board[i,j] == 'Q' and not self.is_valid(i,j):
-          return False
-    return True
-
-
+    
   # do the recursive backtracking
   def recursive_solve (self, col):
     if (col == self.n):
-      return True
+      # add 1 to the count for associated solution
+      self.count += 1
+      #self.print_board()
+      # ends the search after adding 1 to count
+      return
     else:
       for i in range (self.n):
         if (self.is_valid (i, col)):
           self.board[i][col] = 'Q'
-          if (self.recursive_solve(col + 1)):
-            return True
+          # find all other solutions for all cols
+          self.recursive_solve(col + 1)
           self.board[i][col] = '*'
-      return False
-  
-  def rotate(self):
-    n = self.n
-    # Transpose the matrix
-    for i in range(n):
-      for j in range(i, n):
-        self.board[i][j], self.board[j][i] = self.board[j][i], self.board[i][j]
-    # Reverse each row of the transposed matrix
-    for i in range(n):
-      self.board[i] = self.board[i][::-1]
-    return self.board
-  
-  def flipx(self):
-    n = self.n
-    # Swap the rows of the matrix
-    for i in range(n // 2):
-      self.board[i], self.board[n-i-1] = self.board[n-i-1], self.board[i]
-    return self.board
-
-  def flipy(self):
-    n = self.n
-    # Swap the columns of the matrix
-    for i in range(n):
-      for j in range(n // 2):
-        self.board[i][j], self.board[i][n-j-1] = self.board[i][n-j-1], self.board[i][j]
-    return self.board
-  
-  def find_non_distinct_solutions(self):
-    # count is 1 because there is 1 solution already
-    count = 1
-    original_board = self.board
-    
-    for i in range(1,4):  
-      self.rotate()
-      if self.is_solution():
-        count += 1
-
-    self.board = original_board
-    self.flipx()
-    
-    if self.is_solution():
-      count+=1
-    
-    self.board = original_board
-    self.flipy()
-    
-    if self.is_solution():
-      count+=1
-    self.board = original_board
-  
-  def find_all_solutions(self, distinct_solutions):
-    solutions = 0
-    for x in distinct_solutions:
-      self.board = x
-      solutions += self.find_non_distinct_solutions
-    return solutions
-
-    
-
 
   # if the problem has a solution print the board
   def solve (self):
-    for i in range (self.n):
-      if (self.recursive_solve(i)):
-        self.print_board()
+    # start at col 0 and find all solutions from there
+    self.recursive_solve(0)
 
 def main():
   # read the size of the board
@@ -152,7 +87,7 @@ def main():
   game.solve()
 
   # print the number of solutions
+  print(game.count)
  
 if __name__ == "__main__":
   main()
-
