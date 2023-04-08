4b93ae8aa18f89b9d92ff00ec22ec9fd926e3b95
ssidd02 <simaaksidd1@gmail.com>
2023-04-07 23:06:31 -0500

    finish A18

--- A18/ExpressionTree.py
+++ A18/ExpressionTree.py
@@ -38,6 +38,7 @@ class Stack (object):
     def is_empty(self):
         return len(self.stack) == 0
 
+
 class Node (object):
     def __init__ (self, data = None, lChild = None, rChild = None):
         self.data = data
@@ -73,31 +74,44 @@ class Tree (object):
                 stack.push(cur_node)
                 cur_node.rChild = Node()
                 cur_node = cur_node.rChild
-            # cur_node's data = operand. 
-            # cur_node = parent by popping the stack. 
-            elif tok.isdigit():
-                cur_node.data = tok
-                cur_node = stack.pop()
             # make cur_node = parent node 
             # by popping the stack if it is not empty.
             elif tok == ')':
                 if not stack.is_empty():
                     cur_node = stack.pop()
-
+            # cur_node's data = operand. 
+            # cur_node = parent by popping the stack. 
+            else:
+                cur_node.data = tok
+                cur_node = stack.pop()
+    def operate(self, op, right, left):
+        if op == '//':
+            return right // left
+        if op == '**':
+            return right ** left
+        if op == '*':
+            return right * left
+        if op == '%':
+            return right % left
+        if op == '+':
+            return right + left
+        if op == '-':
+            return right - left
+        if op == '*':
+            return right * left
+        if op == '/':
+            return right / left
     
     # this function should evaluate the tree's expression
     # returns the value of the expression after being calculated
     def evaluate (self, aNode):
-        stack = Stack()
-        stack.push(aNode)
-        while not stack.is_empty():
-            cur_node = stack.pop()
-            if cur_node.isleaf():
-                stack.push(int(cur_node.data))
-            else:
-                ...
-                
-        
+        if aNode == None:
+            return 0
+        if aNode.isLeaf():
+            return float(aNode.data)
+        left = self.evaluate(aNode.lChild)
+        right = self.evaluate(aNode.rChild)
+        return self.operate(aNode.data, left, right)
     
     # this function should generate the preorder notation of 
     # the tree's expression
@@ -128,7 +142,14 @@ class Tree (object):
                 postorder += cur_node.data + ' '
                 stack.push(cur_node.lChild)
                 stack.push(cur_node.rChild)
-        return postorder[::-1]
+        postorder = postorder.split()
+        for i in range(len(postorder)):
+            if '.' in postorder[i]:
+                pass
+            else:
+                postorder[i] = postorder[i][::-1]
+        reverse = ' '.join(postorder[::-1])
+        return reverse
 
 
 # you should NOT need to touch main, everything should be handled for you
@@ -139,7 +160,6 @@ def main():
  
     tree = Tree()
     tree.create_tree(expr)
-    
     # evaluate the expression and print the result
     print(expr, "=", str(tree.evaluate(tree.root)))
 
